7b2a1ddd4f63f59793697395ba1a162b541ed27e
Marc  McCue <marcdmccue@gmail.com>
2021-02-19 11:59:32 -0500

    update

---
+++ src/pages/About.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        I'm baby migas live-edge kitsch vinyl bushwick organic williamsburg
+        shoreditch cardigan. Skateboard whatever hashtag migas squid drinking
+        vinegar thundercats copper mug lyft ennui microdosing. You probably
+        haven't heard of them man bun everyday carry synth mustache live-edge
+        art party enamel pin taxidermy. Ugh crucifix thundercats mustache subway
+        tile adaptogen health goth iceland banh mi cold-pressed pitchfork echo
+        park gluten-free actually hoodie. Pitchfork affogato post-ironic
+        kombucha glossier tilde photo booth.
+      </p>
+      <p>
+        Hashtag pok pok viral fingerstache microdosing umami tumeric four dollar
+        toast. Next level meditation brooklyn portland sartorial, literally
+        adaptogen artisan 3 wolf moon yr edison bulb flannel. Locavore aesthetic
+        vaporware beard echo park, deep v selvage taiyaki neutra blog mustache
+        chambray. Vexillologist austin vape direct trade taiyaki messenger bag
+        cloud bread YOLO adaptogen occupy. Palo santo air plant kinfolk direct
+        trade cardigan poke craft beer pop-up intelligentsia pickled authentic
+        fashion axe gluten-free. Fam DIY paleo deep v copper mug, cold-pressed
+        humblebrag raw denim try-hard.
+      </p>
+    </section>
+  );
+};
+
+export default About;
